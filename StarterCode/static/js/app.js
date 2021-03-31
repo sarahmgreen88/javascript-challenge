a856ca1ca8ab1485f40e48bac576fa5c4bca1168
unknown <sarahmgreen88@gmail.com>
2021-03-31 07:58:57 -0400

    updating code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,35 @@
 // from data.js
 var tableData = data;
-
+var tbody = d3.select('tbody');
 // YOUR CODE HERE!
+console.log(tableData)
+
+tableData.forEach((UFOsighting) => {
+    var row = tbody.append('tr');
+    Object.entries(UFOsighting).forEach(([key,value]) => {
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData)
+    // Use the form input to filter the data by blood type
+  
+    var filteredData = tableData.filter(date => date.Date === inputValue);
+    console.log(filteredData);
+}
